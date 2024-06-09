6550549d3beb88d1732e006767583ce4cab79138
Stanley Lew <stanley43595@gmail.com>
2024-06-08 17:57:01 -0700

    colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,16 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      color: {
+        witl: {
+          "pink-100": "#F8DFDF",
+          "purple-100": "#572B6C",
+          "purple-200": "#D0BCE0",
+          "yellow-100": "#FFF6EB",
+        },
+      },
+    },
   },
   plugins: [],
 };
