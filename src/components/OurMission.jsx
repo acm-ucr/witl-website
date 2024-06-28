8e6c3a33ec0d4b2762e528775c85e8210b92087d
felix diaz <felixmdm321@gmail.com>
2024-06-27 22:35:03 -0700

    formatting failing

--- src/components/OurMission.jsx
+++ src/components/OurMission.jsx
@@ -1,9 +1,9 @@
 const OurMission = () => {
-    return (
-        <div className="flex flex-row">
-            <p className="m-auto p-auto font-serif text-7xl">Our Mission</p>
-        </div>
-    );
+  return (
+    <div className="flex flex-row">
+      <p className="m-auto p-auto font-serif text-7xl">Our Mission</p>
+    </div>
+  );
 };
 
 export default OurMission;
