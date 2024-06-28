da9386762c1a6a0ffe68d96cd065235299092f72
Kaylie Zhao <zhaokaylie@gmail.com>
2024-06-27 19:01:50 -0700

    purple-300 shows gray on my screen

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,18 @@
+import Link from "next/link";
+
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div>
+      404 Page Not Found
+      <br /> <br />
+      <Link
+        href="/"
+        className="bg-witl-purple-100 text-witl-purple-300 text-xl px-24 py-4 rounded-xl"
+      >
+        Back To Home
+      </Link>
+    </div>
+  );
 };
 
 export default NotFound;
