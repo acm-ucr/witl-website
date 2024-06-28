7ecb18482ae98ed9c8ed4482ffd628b865ed7e77
Kaylie Zhao <zhaokaylie@gmail.com>
2024-06-28 11:50:40 -0700

    I added the back to home button for the 404 not found page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -4,13 +4,14 @@ const NotFound = () => {
   return (
     <div>
       404 Page Not Found
-      <br /> <br />
-      <Link
-        href="/"
-        className="bg-witl-purple-100 text-witl-purple-300 text-xl px-24 py-4 rounded-xl"
-      >
-        Back To Home
-      </Link>
+      <div class="flex flex-col items-center justify-center ">
+        <Link
+          href="/"
+          className="bg-witl-white-100 text-witl-purple-300 text-xl px-24 py-4 rounded-xl"
+        >
+          Back To Home
+        </Link>
+      </div>
     </div>
   );
 };
