7d2e03c512210c0dd6fe7844c36a711e084dc08e
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-06-27 20:14:13 -0700

    check code, theres an error

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 /* eslint-disable new-cap */
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Navigation from "@/components/Navigation.jsx";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navigation>{children}</Navigation>
+      </body>
     </html>
   );
 }
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,4 +1,23 @@
+// import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import navLogo from "@/../public/images/WITL-logo.webp";
+import Image from "next/image";
+
 const Navigation = () => {
-  return <div></div>;
+  return (
+    <Navbar expand="md" className="bg-witl-purple-200">
+      <Container>
+        <Navbar.Brand href="/">
+          <Image src={navLogo} alt="WITL logo" />
+          <div className="">
+            <div>WOMXN IN THE LAW</div>
+            <div>EST. 2022</div>
+          </div>
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
+  );
 };
+
 export default Navigation;
