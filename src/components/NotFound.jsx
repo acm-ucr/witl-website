d4ccfd91ecb2b195bd771b3521a8952578b5827a
Kaylie Zhao <zhaokaylie@gmail.com>
2024-06-29 12:17:51 -0700

    added to notfound jsx, changed font & bg

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -7,7 +7,7 @@ const NotFound = () => {
       <div className="flex flex-col items-center justify-center rounded-2xl">
         <Link
           href="/"
-          className="bg-witl-white-100 text-witl-purple-300 text-2xl font-medium	px-24 py-4 rounded-2xl	"
+          className="bg-witl-white-100 text-witl-purple-300 text-2xl font-medium px-24 py-4 rounded-2xl	"
         >
           Back To Home
         </Link>
