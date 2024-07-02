b2b88821d51b2db729a23d5248ffcfb29eabfecb
Andrew Trang <andrewtrang@Andrews-MacBook-Air-6.local>
2024-07-01 20:23:09 -0700

    added 404

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -2,12 +2,14 @@ import Link from "next/link";
 
 const NotFound = () => {
   return (
-    <div className="bg-witl-purple-400 min-h-screen font-marcellus-sc">
+    <div className="bg-witl-purple-400 min-h-screen font-marcellus-sc flex flex-col justify-center items-center">
       <div className="text-witl-white-100 font-libre-caslon-text3 text-3xl pb-1 text-center">
         <p>Page Not Found</p>
       </div>
-      <div className="flex flex-col bg-witl-purple-300 w-min px-52 py-11
-      rounded-2xl text-witl-purple-100 text-7xl font-libre-caslon-text items-center justify-center">
+      <div
+        className="flex flex-col bg-witl-purple-300 w-13 px-52 py-6
+      rounded-2xl text-witl-purple-100 text-9xl font-libre-caslon-text items-center justify-center"
+      >
         <p>404</p>
       </div>
       <div className="flex flex-col items-center justify-center rounded-2xl">
