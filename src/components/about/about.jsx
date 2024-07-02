e5dfd476ccc0c528c6f2c2f372165b9e1eb4cb4a
justin-h-im <imjustin679@gmail.com>
2024-07-01 17:51:35 -0700

    Corrected colors displayed on the page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,11 +1,11 @@
 const About = () => {
   return (
-    <div className="bg-purple-300 min-h-screen flex items-center justify-center">
+    <div className="bg-witl-purple-300 min-h-screen flex items-center justify-center">
       <div className="flex flex-col tracking-widest">
-        <div className="mb-1 text-9xl text-center text-purple-200">
+        <div className="mb-1 text-9xl text-center text-witl-purple-100">
           <p>ABOUT</p>
         </div>
-        <div className="mb-1 text-9xl text-center text-purple-200">
+        <div className="mb-1 text-9xl text-center text-witl-purple-100">
           <p>US</p>
         </div>
       </div>
