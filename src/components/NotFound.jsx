8f528eb449bdb9d36a5de921c282b9555244ecdd
Cathy Wu <cathywu@Cathys-MacBook-Air.local>
2024-07-01 22:18:28 -0700

    changed to tailwind

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -4,10 +4,7 @@ const NotFound = () => {
   return (
     <div className="bg-witl-purple-400 min-h-screen font-marcellus-sc">
       404 Page Not Found
-      <p
-        className="text-witl-white-100 text-4xl mb-8 flex flex-col items-center justify-center"
-        style={{ paddingBottom: "60px" }}
-      >
+      <p className="text-witl-white-100 text-4xl mb-8 flex flex-col items-center justify-center pb-16">
         Adjective Adjective Adjective
       </p>
       <div className="flex flex-col items-center justify-center rounded-2xl">
