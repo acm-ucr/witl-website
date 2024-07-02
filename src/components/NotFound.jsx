e6fb67c35676053ae5bb2c83418c422a09ddd569
Cathy Wu <cathywu@Cathys-MacBook-Air.local>
2024-07-01 17:00:35 -0700

    cathy/pageNotFound

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -5,6 +5,9 @@ const NotFound = () => {
     <div className="bg-witl-purple-400 min-h-screen font-marcellus-sc">
       404 Page Not Found
       <div className="flex flex-col items-center justify-center rounded-2xl">
+        <p className="text-witl-white-100 text-4xl mb-8">
+          Adjective Adjective Adjective
+        </p>
         <Link
           href="/"
           className="bg-witl-white-100 text-witl-purple-300 text-2xl font-medium px-24 py-4 rounded-2xl	"
