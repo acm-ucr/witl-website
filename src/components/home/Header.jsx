8f48e20c4b54252b8cb9583e862869bfff913ac3
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-07-04 15:39:21 -0700

    center header

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -3,26 +3,28 @@ import HeaderImg from "@/public/assets/mainHeader.webp";
 
 const Header = () => {
   return (
-    <div className="text-witl-purple-100 w-[85%]">
-      <div className="flex flex-row">
-        <p className="text-[13rem] leading-tight">WOMXN</p>
-        <div className="flex flex-col w-[25%] justify-end">
-          <p className="text-5xl mb-[3%]">IN THE</p>
-          <p className="mb-[15%]">
-            University of California, Riverside&apos;s undergraduate community
-            passionate about law and the pursuit of justice.
-          </p>
+    <div className="flex justify-center">
+      <div className="text-witl-purple-100 w-[75%]">
+        <div className="flex flex-row">
+          <p className="text-[13rem] leading-tight">WOMXN</p>
+          <div className="flex flex-col w-[25%] justify-end">
+            <p className="text-5xl mb-[3%]">IN THE</p>
+            <p className="mb-[15%]">
+              University of California, Riverside&apos;s undergraduate community
+              passionate about law and the pursuit of justice.
+            </p>
+          </div>
         </div>
-      </div>
-      <div className="flex flex-row">
-        <div className="w-[60%]">
-          <Image
-            src={HeaderImg}
-            alt="Header Image"
-            className="w-[95%] h-auto"
-          />
+        <div className="flex flex-row">
+          <div className="w-[60%]">
+            <Image
+              src={HeaderImg}
+              alt="Header Image"
+              className="w-[95%] h-auto"
+            />
+          </div>
+          <p className="text-[13rem] w-[30%] leading-none mt-[-1.5%]">LAW</p>
         </div>
-        <p className="text-[13rem] w-[30%] leading-none mt-[-1.5%]">LAW</p>
       </div>
     </div>
   );
