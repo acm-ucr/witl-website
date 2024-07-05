ed082ffd8e998ff37ea61aef751d1310906bb70f
AxelPere <Paxel1001@gmail.com>
2024-07-05 12:04:32 -0700

    added component to board page and changed components to use tailwind style instead of CSS

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -5,10 +5,7 @@ const Board = () => {
   return (
     <div className="bg-witl-purple-300 flex flex-col pb-40">
       <Image src={BoardIMG} alt="Board" className="self-end mr-10 mt-10" />
-      <p
-        className="text-witl-purple-100 w-min text-8xl absolute z-0"
-        style={{ bottom: "4%", left: "9%" }}
-      >
+      <p className="text-witl-purple-100 w-min text-8xl absolute z-0 top-[60%] left-[9%]">
         EXECUTIVE BOARD
       </p>
     </div>
