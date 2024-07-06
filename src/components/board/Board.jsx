54c5dd78225622ededcca18ce14565c1bbe01dbc
felix diaz <felixmdm321@gmail.com>
2024-07-05 19:09:21 -0700

    added alumni board + descriptor text

--- src/components/board/Board.jsx
+++ src/components/board/Board.jsx
@@ -8,4 +8,3 @@ const Board = () => {
   );
 };
 export default Board;
-  
--- src/components/board/OurAlumni.jsx
+++ src/components/board/OurAlumni.jsx
@@ -10,28 +10,28 @@ const OurAlumni = () => {
   return (
     <div className="grid grid-rows-2 grid-cols-4 gap-4 mx-20 m-4">
       <div className="place-content-center m-auto">
-      <Image src={One} alt="Alumni 1"/>
+        <Image src={One} alt="Alumni 1" />
       </div>
       <div className="place-content-center m-auto">
-        <Image src={Two} alt="Alumni 2"/>
+        <Image src={Two} alt="Alumni 2" />
       </div>
       <div className="place-content-center m-auto">
-        <Image src={Three} alt="Alumni 3"/>
+        <Image src={Three} alt="Alumni 3" />
       </div>
       <div className="place-content-center m-auto">
-        <Image src={Four} alt="Alumni 4"/>
+        <Image src={Four} alt="Alumni 4" />
       </div>
       <div className="place-content-center m-auto">
-        <Image src={Five} alt="Alumni 5"/>
+        <Image src={Five} alt="Alumni 5" />
       </div>
       <div className="place-content-center m-auto">
-        <Image src={Six} alt="Alumni 6"/>
+        <Image src={Six} alt="Alumni 6" />
       </div>
       <div className="col-span-2 bg-witl-purple-400 mx-4 place-content-center">
         <div className="text-right text-witl-white-100 text-[120px]">
-            Our
-            <br />
-            Alumni
+          Our
+          <br />
+          Alumni
         </div>
       </div>
     </div>
