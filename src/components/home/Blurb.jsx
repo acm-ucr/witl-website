3b6a2a45b364c10b0b1deabfed11bcb6a0fc1bf5
justin-h-im <imjustin679@gmail.com>
2024-07-06 22:20:19 -0700

    created main page blurb component file

---
+++ src/components/home/Blurb.jsx
@@ -0,0 +1,12 @@
+const Blurb = () => {
+  return (
+    <div>
+      <div className="grid cols-2">
+        <div>Hello World</div>
+        <div>Hell again</div>
+      </div>
+    </div>
+  );
+};
+
+export default Blurb;
