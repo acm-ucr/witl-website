7e54a04418480ea7367ff46ce59ad43742875d5d
Axel Perez <134023272+AxelPere@users.noreply.github.com>
2024-07-07 13:36:14 -0700

    Make component slightly longer for text

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -3,7 +3,7 @@ import BoardIMG from "@/public/board/Exec_Board.webp";
 
 const BoardHeader = () => {
   return (
-    <div className="bg-witl-purple-300 flex flex-col pb-40">
+    <div className="bg-witl-purple-300 flex flex-col pb-56">
       <Image src={BoardIMG} alt="Board" className="self-end mr-10 mt-10" />
       <p className="text-witl-purple-100 w-min text-8xl absolute z-0 top-[60%] left-[9%]">
         EXECUTIVE BOARD
