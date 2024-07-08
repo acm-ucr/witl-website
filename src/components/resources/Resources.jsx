ee5ba05c641a648ece41c1129cb8119d953261bc
Stanley Lew <stanley43595@gmail.com>
2024-07-08 16:33:16 -0700

    moved around files and folder names

--- src/components/resources/Resources.jsx
+++ src/components/resources/Resources.jsx
@@ -1,8 +1,8 @@
 import Link from "next/link";
 import Image from "next/image";
-import FellowshipImage from "@/public/images/fellowship.webp";
-import NetworkDinner from "@/public/images/networkDinner.webp";
-import GroupPhoto from "@/public/images/groupPhoto.webp";
+import FellowshipImage from "@/public/resources/fellowship.webp";
+import NetworkDinner from "@/public/resources/networkDinner.webp";
+import GroupPhoto from "@/public/resources/groupPhoto.webp";
 
 const Resources = () => {
   return (
