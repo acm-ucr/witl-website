8525d32069b240bb7471a4a9c695d0aa0334c10f
justin-h-im <imjustin679@gmail.com>
2024-07-08 15:55:17 -0700

    added component call to main page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Header from "@/components/home/Header.jsx";
+import Blurb from "@/components/home/Blurb.jsx";
 const Home = () => {
   return (
     <div>
       <p>Hello World</p>
       <Header />
+      <Blurb />
     </div>
   );
 };
