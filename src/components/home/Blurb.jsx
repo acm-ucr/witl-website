4894f88ff17d3f7640daf57f7e13fd1f6df1bf20
justin-h-im <imjustin679@gmail.com>
2024-07-07 23:06:29 -0700

    preliminary draft on main page blurb. next steps are to pack everything together more tightly

--- src/components/home/Blurb.jsx
+++ src/components/home/Blurb.jsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
+import teamphoto1 from "@/public/home/teamphoto1.webp";
+import teamphoto2 from "@/public/home/teamphoto2.webp";
+import teamphoto3 from "@/public/home/teamphoto3.webp";
+
 const Blurb = () => {
   return (
-    <div>
-      <div className="grid cols-2">
-        <div>Hello World</div>
-        <div>Hell again</div>
+    <div className="bg-witl-purple-300 min-h-screen flex justify-center items-center">
+      <div className="bg-witl-white-100 rounded-3xl">
+        <div className="grid grid-flow-col grid-cols-3 grid-rows-2 content-stretch">
+          <div className="col-span-1">
+            <Image src={teamphoto1} alt="teamphoto1" className="scale-[90%]" />
+          </div>
+          <div>
+            <Image src={teamphoto2} alt="teamphoto2" className="scale-[80%]" />
+          </div>
+          <div className="col-span-3">hello world</div>
+          <div className="col-span-3">
+            <Image src={teamphoto3} alt="teamphoto3" className="scale-[80%] " />
+          </div>
+        </div>
       </div>
     </div>
   );
