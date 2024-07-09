461616309fa4f180b5c0c53b14f372b18b571fd9
AxelPere <Paxel1001@gmail.com>
2024-07-08 19:46:19 -0700

    added more percentages to work better on different screens

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -3,9 +3,13 @@ import BoardIMG from "@/public/board/Exec_Board.webp";
 
 const BoardHeader = () => {
   return (
-    <div className="bg-witl-purple-300 flex flex-col pb-40">
-      <Image src={BoardIMG} alt="Board" className="self-end mr-10 mt-10" />
-      <p className="text-witl-purple-100 w-min text-8xl absolute z-0 top-[60%] left-[9%]">
+    <div className="bg-witl-purple-300 flex flex-col pb-[15%]">
+      <Image
+        src={BoardIMG}
+        alt="Board"
+        className="self-end mr-[3%] mt-[3%] w-[60%]"
+      />
+      <p className="text-witl-purple-100 w-min absolute text-9xl leading-none z-0 top-[58%] left-[10%]">
         EXECUTIVE BOARD
       </p>
     </div>
