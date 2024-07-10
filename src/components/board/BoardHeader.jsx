5f3382ecdabb4d7d0695c326f0f057b83e126657
Stanley Lew <stanley43595@gmail.com>
2024-07-09 22:46:43 -0700

    fixed %

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -5,7 +5,7 @@ const BoardHeader = () => {
   return (
     <div className="bg-witl-purple-300 flex flex-col pb-56">
       <Image src={BoardIMG} alt="Board" className="self-end mr-10 mt-10" />
-      <p className="text-witl-purple-100 w-min text-8xl absolute z-0 top-[60%] left-[9%]">
+      <p className="text-witl-purple-100 w-min text-8xl absolute z-0 top-[25%] left-[15%]">
         EXECUTIVE BOARD
       </p>
     </div>
