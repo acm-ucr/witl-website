43a26d5ab11c69f9c61ad25660682c3cdbe794f1
hanley76129 <hanhuynh3422@gmail.com>
2024-07-11 15:35:13 -0700

    hanley/setbanner

--- src/components/home/Sponsors.jsx
+++ src/components/home/Sponsors.jsx
@@ -1,6 +1,6 @@
 const Sponsors = () => {
   return (
-    <div className="flex flex-col bg-witl-purple-300 text-3xl text-white font-bold">
+    <div className="flex flex-col bg-witl-purple-300 text-[50px] text-white font-bold">
       <div className="w-1/2 bg-witl-purple-400 mb-6 mt-4 font-libre-caslon-text py-2 text-right pr-5">
         BEST BEST KRIEGER
       </div>
@@ -9,7 +9,7 @@ const Sponsors = () => {
           HQR INC.
         </div>
       </div>
-      <div className="border-witl-purple-400 border-b-8 ">
+      <div className="border-witl-purple-400 border-b-[21px] ">
         <div className="text-witl-purple-400 text-sm mb-1 font-Junge">
           Events
         </div>
