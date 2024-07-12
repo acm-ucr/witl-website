407d77bda8cadb08b447297dde659d3a325b5bbc
felix diaz <felixmdm321@gmail.com>
2024-07-11 23:49:47 -0700

    added group pics beneath about us

---
+++ src/components/about/AboutUsGroups.jsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import left from "@/public/about/grouppic.webp";
+import center from "@/public/about/grouppic3.webp";
+import right from "@/public/about/grouppic4.webp";
+
+const AboutUsGroups = () => {
+    return (
+        <div className="flex flex-box w-[100%] mt-[-100px] z-0">
+            <div className="my-[10%]">
+                <Image src={left} alt="group pic 1" />            
+            </div>
+            <div className="mx-auto object-fit">
+                <Image src={center} alt="group pic 2"/>              
+            </div>
+            <div className="my-[10%]">
+                <Image src={right} alt="group pic 3"/>            
+            </div>
+        </div>
+    );
+};
+export default AboutUsGroups;
