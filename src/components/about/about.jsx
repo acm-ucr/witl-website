033224e27057ed94ecac4057eb38a27d3d3a2a2a
felix diaz <felixmdm321@gmail.com>
2024-07-13 08:58:11 -0600

    rounded the corners on about page

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,7 +2,7 @@ import AboutUsGroups from "./AboutUsGroups";
 
 const About = () => {
   return (
-    <div className="bg-witl-purple-300 min-h-screen flex flex-col items-center justify-center">
+    <div className="bg-witl-purple-300 min-h-screen flex flex-col rounded-b-[48px] items-center justify-center">
       <div className="flex flex-col tracking-widest">
         <div className="mb-1 text-9xl text-center text-witl-purple-100">
           <p>ABOUT</p>
