b0bc70575258a19a54f7d77ced264aa18f4ae9e3
Marco Navarro <duvalincus@gmail.com>
2024-07-14 13:03:12 -0700

    React icons in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,14 +7,19 @@ import { links } from "@/data/Footer";
 const Footer = () => {
   return (
     <div className="bottom-0 p-3 flex items-center bg-witl-purple-300 justify-between">
-      <div className="flex gap-8 justify-center items-center">
+      <div className="flex gap-10 justify-center items-center ml-2">
         {links.map((link, index) => (
-          <Link key={index} href={"#"} target="_blank">
-            <Image src={link.icon} alt={`to ${link.icon}`} width={30} />
+          <Link
+            key={index}
+            href={link.link}
+            target="_blank"
+            className="text-white text-4xl"
+          >
+            {link.icon}
           </Link>
         ))}
       </div>
-      <div className="rounded-full bg-white h-12 aspect-square flex items-center overflow-hidden m-1">
+      <div className="rounded-full bg-white h-12 aspect-square flex items-center overflow-hidden m-1 mr-2">
         <Image src={logo} alt="witl logo" />
       </div>
     </div>
