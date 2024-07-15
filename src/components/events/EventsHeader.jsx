36d165f2867c20bb249101d2f57cbb3e71dca218
Cathy Wu <cathywu@Cathys-MacBook-Air.local>
2024-07-15 13:26:15 -0700

    fixed font color for events page

--- src/components/events/EventsHeader.jsx
+++ src/components/events/EventsHeader.jsx
@@ -11,7 +11,7 @@ const Header = () => {
         className="absolute right-0 top-[10%] my-auto"
         width={1000}
       />
-      <p className="text-9xl text-witl-white-100 absolute bottom-0 p-[1%]">
+      <p className="text-9xl text-witl-purple-100 absolute bottom-0 p-[1%]">
         EVENTS
       </p>
     </div>
