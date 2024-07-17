6c377f71f74a98484127f2016986869e34f60359
AxelPere <Paxel1001@gmail.com>
2024-07-17 13:07:59 -0700

    added responsiveness to blurb

--- src/components/home/Blurb.jsx
+++ src/components/home/Blurb.jsx
@@ -6,9 +6,9 @@ import teamphoto3 from "@/public/home/teamphoto3.webp";
 const Blurb = () => {
   return (
     <div className="bg-witl-purple-300 min-h-[700px] max-h-screen flex justify-center items-center">
-      <div className="bg-witl-white-100 rounded-[6rem] scale-50 -my-[10%]">
+      <div className="bg-witl-white-100 rounded-[6rem] scale-75 lg:scale-50 md:-my-[10%]">
         <div className="grid grid-flow-row-dense grid-cols-6 ">
-          <div className="col-span-6 md:col-span-3 place-items-stretch justify-start m-10 mt-28 max-h-96 max-w-screen">
+          <div className="col-span-3 place-items-stretch justify-start m-5 md:m-10 mt-14 md:mt-28 max-h-96 max-w-screen">
             {" "}
             <Image
               src={teamphoto1}
@@ -16,14 +16,14 @@ const Blurb = () => {
               className="w-full h-full object-cover rounded-3xl"
             />{" "}
           </div>
-          <div className="col-span-6 md:col-span-3 place-items-stretch justify-start max-h-96 max-w-screen">
+          <div className="col-span-3 place-items-stretch justify-start max-h-96 max-w-screen">
             <div className="flex flex-col tracking-widest">
-              <div className="flex flex-col tracking-wide font-marcellus-sc text-8xl mt-28 ml-6 leading-none">
+              <div className="flex flex-col tracking-wide font-marcellus-sc text-4xl md:text-6xl lg:text-8xl mt-28 ml-6 leading-none">
                 Womxn in
                 <br />
                 the Law
               </div>
-              <p className="text-right text-3xl mr-32 mt-8 leading-tight tracking-tight">
+              <p className="text-right text-md md:text-xl lg:text-2xl mr-8 md:mr-16 lg:mr-32 mt-4 lg:mt-8 leading-tight tracking-tight">
                 stands
                 <br />
                 as an organization catered to the <br />
@@ -35,7 +35,7 @@ const Blurb = () => {
               </p>
             </div>
           </div>
-          <div className="col-span-6 md:col-span-2 place-items-stretch justify-start m-10 mb-20 max-h-96 max-w-screen">
+          <div className="col-span-2 place-items-stretch justify-start m-5 md:m-10 mb-10 md:mb-20 max-h-96 max-w-screen">
             {" "}
             <Image
               src={teamphoto2}
@@ -43,12 +43,12 @@ const Blurb = () => {
               className="w-full h-full object-cover rounded-3xl"
             />{" "}
           </div>
-          <div className="col-span-6 md:col-span-4 place-items-stretch justify-start m-10 mb-20 max-h-96 max-w-screen">
+          <div className="col-span-4 place-items-stretch justify-start m-5 md:m-10 my-10 md:mb-20 max-h-96 max-w-screen">
             {" "}
             <Image
               src={teamphoto3}
               alt="teamphoto3"
-              className="w-full h-full object-cover rounded-3xl"
+              className="w-full h-full md:h-full object-cover rounded-3xl"
             />{" "}
           </div>
         </div>
