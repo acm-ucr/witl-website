7ab1e82c715cfdedea4d05007e3b4df5c2f75e37
Stanley Lew <stanley43595@gmail.com>
2024-07-18 22:35:39 -0700

    optimized for laptop screens now, needs to be reoptimized for monitors

--- src/components/about/CoreValues.jsx
+++ src/components/about/CoreValues.jsx
@@ -47,11 +47,11 @@ const CoreValues = () => {
           alt="knowledge"
         />
       </div>
-      <div className="absolute bottom-[10%]">
-        <p className="text-8xl pb-12 pl-20 md:scale-150 font-marcellus-sc">
+      <div className="absolute bottom-[4%]">
+        <p className="text-7xl pb-12 pl-20 md:scale-150 font-marcellus-sc">
           CORE
         </p>
-        <p className="text-8xl pl-20 md:scale-150 font-marcellus-sc">VALUES</p>
+        <p className="text-7xl pl-20 md:scale-150 font-marcellus-sc">VALUES</p>
       </div>
     </div>
   );
