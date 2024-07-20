b04a63b852bbdfb41f7c388079f7908b11300f23
felix diaz <felixmdm321@gmail.com>
2024-07-20 12:09:02 -0600

    resolving merge conflict in AboutHeader

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -2,10 +2,10 @@ const About = () => {
   return (
     <div className="bg-purple-300 min-h-screen flex items-center justify-center">
       <div className="flex flex-col tracking-widest">
-        <div className="mb-1 text-9xl text-center text-purple-200">
+        <div className="mb-1 text-7xl md:text-9xl text-center text-purple-200">
           <p>ABOUT</p>
         </div>
-        <div className="mb-1 text-9xl text-center text-purple-200">
+        <div className="mb-16 md:mb-1 text-7xl md:text-9xl text-center text-purple-200">
           <p>US</p>
         </div>
       </div>
