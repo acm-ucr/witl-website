8898302086315bf354786c68848f215c8061f297
Marco Navarro <duvalincus@gmail.com>
2024-07-22 12:54:43 -0700

    Changed font size and spacing for header and sponsors

--- src/components/home/Header.jsx
+++ src/components/home/Header.jsx
@@ -3,19 +3,21 @@ import HeaderImg from "@/public/assets/mainHeader.webp";
 
 const Header = () => {
   return (
-    <div className="bg-witl-purple-300 flex flex-col items-center">
-      <div className="text-witl-purple-100 w-[78%] mt-32">
-        <div className="flex flex-row">
-          <p className="text-[13rem] leading-tight">WOMXN</p>
+    <div className="bg-witl-purple-300 flex flex-col items-center w-screen">
+      <div className="text-witl-purple-100 w-[78%] lg:mt-[10%] mt-[22%]">
+        <div className="flex flex-row items-center justify-center">
+          <p className="lg:text-[13rem] leading-tight text-5xl md:text-7xl">
+            WOMXN
+          </p>
           <div className="flex flex-col w-[25%] justify-end">
-            <p className="text-5xl mb-[3%]">IN THE</p>
-            <p className="mb-[15%]">
+            <p className="lg:text-5xl mb-[3%] text-lg md:text-3xl">IN THE</p>
+            <p className="mb-[15%] text-[0.3rem] lg:text-sm md:text-[0.5rem]">
               University of California, Riverside&apos;s undergraduate community
               passionate about law and the pursuit of justice.
             </p>
           </div>
         </div>
-        <div className="flex flex-row">
+        <div className="flex flex-row justify-center mb-2">
           <div className="w-[60%]">
             <Image
               src={HeaderImg}
@@ -23,7 +25,9 @@ const Header = () => {
               className="w-[95%] h-auto"
             />
           </div>
-          <p className="text-[13rem] w-[30%] leading-none mt-[-1.5%]">LAW</p>
+          <p className="lg:text-[13rem] w-[30%] leading-none mt-[-1.5%] text-5xl md:text-7xl">
+            LAW
+          </p>
         </div>
       </div>
     </div>
