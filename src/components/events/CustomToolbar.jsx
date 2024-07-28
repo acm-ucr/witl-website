1e4b9ff64fe753cafaaf40f835eec77ff13d8dd0
Mailyn Nguyen <ngu.mailyn.yen@gmail.com>
2024-07-28 14:01:10 -0700

    calendar mobile response

--- src/components/events/CustomToolbar.jsx
+++ src/components/events/CustomToolbar.jsx
@@ -1,4 +1,5 @@
 import { FaArrowRightLong, FaArrowLeftLong } from "react-icons/fa6";
+import { useMediaQuery } from "react-responsive";
 
 const monthNames = [
   "January",
@@ -16,27 +17,57 @@ const monthNames = [
 ];
 
 const CustomToolbar = (event) => {
+  const isMobile = useMediaQuery({ query: "(max-width: 768px)"});
   return (
-    <div className="flex justify-between text-8xl mb-[1%] font-junge">
+    <>
+  {isMobile
+      ? (<div className="flex justify-center mb-[1%] font-junge">
+        <div className="flex items-center">
+          <FaArrowLeftLong
+            onClick={() => {
+              event.onNavigate("PREV");
+            }}
+            className="hover:cursor-pointer hover:scale-125 duration-300 transition ease-in-out mr-[4%] text-4xl"
+          />
+          <div className="flex justify-center text-5xl">
+            <p className="mr-4">{monthNames[event.date.getMonth()]}</p>
+            <p>{event.date.getFullYear()}</p>
+          </div>
+          
+          <FaArrowRightLong
+            onClick={() => {
+              event.onNavigate("NEXT");
+            }}
+            className="hover:cursor-pointer hover:scale-125 duration-300 transition ease-in-out ml-[4%] text-4xl"
+          />
+        </div>
+        <div className="flex items-center">
+          
+        </div>
+      </div>)
+    : (<div className="flex justify-between text-7xl xl:text-8xl mb-[1%] font-junge">
       <div className="flex items-center">
         <FaArrowLeftLong
           onClick={() => {
             event.onNavigate("PREV");
           }}
-          className="hover:cursor-pointer hover:scale-125 duration-300 tranistion ease-in-out mr-[12%]"
+          className="hover:cursor-pointer hover:scale-125 duration-300 transition ease-in-ou mr-[4%] text-7xl"
         />
-        <p className="">{monthNames[event.date.getMonth()]}</p>
+        <p>{monthNames[event.date.getMonth()]}</p>
         <FaArrowRightLong
           onClick={() => {
             event.onNavigate("NEXT");
           }}
-          className="hover:cursor-pointer hover:scale-125 duration-300 tranistion ease-in-out ml-[12%] text-8xl"
+          className="hover:cursor-pointer hover:scale-125 duration-300 transition ease-in-out ml-[4%] text-7xl"
         />
       </div>
       <div className="flex items-center">
-        <p className="">{event.date.getFullYear()}</p>
+        <p>{event.date.getFullYear()}</p>
       </div>
-    </div>
+    </div>)
+    }
+    </>
+    
   );
 };
 
