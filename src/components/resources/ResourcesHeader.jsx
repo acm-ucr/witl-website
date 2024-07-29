22daf2a783050dc6df0ce3e45efa291c7b294407
hanley76129 <hanhuynh3422@gmail.com>
2024-07-29 08:08:30 -0700

    animatebannerinitial

--- src/components/resources/ResourcesHeader.jsx
+++ src/components/resources/ResourcesHeader.jsx
@@ -1,3 +1,7 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+
 import TopResourcesBanner from "./TopResourcesBanner";
 import BottomResourcesBanner from "./BottomResourcesBanner";
 
@@ -9,40 +13,66 @@ const ResourcesHeader = () => {
   return (
     <div className="bg-witl-purple-300 h-[300px] md:h-screen flex flex-col items-center justify-center mt-[5%]">
       <div className="grid grid-cols-5 gap-4 md:gap-8 mt-[20%] md:mt-[0%] w-10/12">
-        <div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <TopResourcesBanner
             icon={<FaInstagram />}
             link={"https://www.instagram.com/ucrwitl/"}
           />
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <BottomResourcesBanner
             icon={<FaLinkedin />}
             link={"https://www.linkedin.com/company/ucr-womxn-in-the-law/"}
           />
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <TopResourcesBanner
             icon={<MdMail />}
             link={"mailto:ucrwitl@gmail.com"}
           />
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <BottomResourcesBanner
             icon={<FaDiscord />}
             link={"https://discord.gg/6W8t7a3G58"}
           />
-        </div>
-        <div>
+        </motion.div>
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
           <TopResourcesBanner
             icon={<BsSubstack />}
             link={"https://ucrwitl.substack.com/"}
           />
-        </div>
+        </motion.div>
       </div>
-      <div className="absolute text-[25px] md:text-[85px] lg:text-[100px] 2xl:text-[150px] w-[70%] text-center text-witl-white-100 bg-witl-purple-300 mt-[5%] md:mb-[22%]">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="absolute text-[25px] md:text-[85px] lg:text-[100px] 2xl:text-[150px] w-[70%] text-center text-witl-white-100 bg-witl-purple-300 mt-[5%] md:mb-[22%]"
+      >
         <p>RESOURCES</p>
-      </div>
+      </motion.div>
+
       <p className="text-center text-xs md:text-2xl text-witl-white-100  border-y-2 w-full mt-[3%]">
         CHECKOUT OUR RESOURCE SHEET
       </p>
