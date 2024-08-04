85898d3d4b352981ef880f88c151922d37485a99
felix diaz <felixmdm321@gmail.com>
2024-08-04 00:17:34 -0700

    formatting

--- src/components/about/OurMission.jsx
+++ src/components/about/OurMission.jsx
@@ -17,7 +17,7 @@ const animate = {
 const OurMission = () => {
   return (
     <div className="py-[7vh]">
-      <motion.div 
+      <motion.div
         variants={animate}
         initial="start"
         whileInView="end"
@@ -29,37 +29,40 @@ const OurMission = () => {
         </p>
       </motion.div>
       <div className="flex justify-center">
-        <motion.div 
+        <motion.div
           variants={animate}
           initial="start"
           whileInView="end"
           transition={{ delay: 0.25 }}
-          className="grid grid-cols-1 md:grid-cols-2 bg-witl-purple-300 rounded-2xl text-witl-purple-100 w-10/12 2xl:w-8/12">
-            <motion.div 
-              variants={animate}
-              initial="start"
-              whileInView="end"
-              transition={{ delay: 0.45 }}
-              className="flex items-center pl-4 py-4 pr-6 md:pl-11 md:py-5 text-[2.5vw] md:text-[1.75vw] lg:text-[1.5vw] xl:text-[1.4vw] 2xl:text-[1.1vw] text-center">
-                Womxn in the Law is UC Riverside’s undergraduate community
-                passionate about law and the pursuit of justice. We aim to create a
-                supportive community and network for diverse womxn pursuing legal
-                careers. In creating a supportive community, establishing a robust
-                network of legal professionals, judges, law school faculty, and
-                accomplished attorneys, Womxn in the Law amplifies the needs of
-                UCR’s most disadvantaged student population. <br />
-                The X in womxn honors women’s intersecting identities, as women in
-                America are impacted by a multitude of social justice and human
-                rights issues. - Kimberlé Crenshaw
-            </motion.div>
-            <motion.div 
-              variants={animate}
-              initial="start"
-              whileInView="end"
-              transition={{ delay: 0.45 }}
-              className="flex justify-center items-center p-8">
-                <Image src={Pic} alt="grouppic2" />
-            </motion.div>
+          className="grid grid-cols-1 md:grid-cols-2 bg-witl-purple-300 rounded-2xl text-witl-purple-100 w-10/12 2xl:w-8/12"
+        >
+          <motion.div
+            variants={animate}
+            initial="start"
+            whileInView="end"
+            transition={{ delay: 0.45 }}
+            className="flex items-center pl-4 py-4 pr-6 md:pl-11 md:py-5 text-[2.5vw] md:text-[1.75vw] lg:text-[1.5vw] xl:text-[1.4vw] 2xl:text-[1.1vw] text-center"
+          >
+            Womxn in the Law is UC Riverside’s undergraduate community
+            passionate about law and the pursuit of justice. We aim to create a
+            supportive community and network for diverse womxn pursuing legal
+            careers. In creating a supportive community, establishing a robust
+            network of legal professionals, judges, law school faculty, and
+            accomplished attorneys, Womxn in the Law amplifies the needs of
+            UCR’s most disadvantaged student population. <br />
+            The X in womxn honors women’s intersecting identities, as women in
+            America are impacted by a multitude of social justice and human
+            rights issues. - Kimberlé Crenshaw
+          </motion.div>
+          <motion.div
+            variants={animate}
+            initial="start"
+            whileInView="end"
+            transition={{ delay: 0.45 }}
+            className="flex justify-center items-center p-8"
+          >
+            <Image src={Pic} alt="grouppic2" />
+          </motion.div>
         </motion.div>
       </div>
     </div>
@@ -67,3 +70,5 @@ const OurMission = () => {
 };
 
 export default OurMission;
+
+
