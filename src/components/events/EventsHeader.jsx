2c52a15c16bf99c5cd2e02071fe0b618bd649e41
Andrew Trang <andrewtrang@wifi-10-40-121-157.wifi.berkeley.edu>
2024-08-05 16:55:08 -0700

    added animation for event header

--- src/components/events/EventsHeader.jsx
+++ src/components/events/EventsHeader.jsx
@@ -1,8 +1,15 @@
+"use client";
 import React from "react";
 import networkingdinner from "@/public/images/networkingdinner.webp";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
-const Header = () => {
+const animation = {
+  hidden: { opacity: 0, x: -30 },
+  show: { opacity: 1, x: 0 },
+};
+
+function Header() {
   return (
     <div className="bg-witl-purple-300 flex justify-end relative mt-[5%] py-[5%]">
       <Image
@@ -10,11 +17,17 @@ const Header = () => {
         alt="networking dinner"
         className="w-[70%] h-auto pt-16 md:pt-0 object-cover"
       />
-      <p className="text-witl-purple-100 text-5xl md:text-9xl absolute left-[4%] top-[65%] md:top-[50%] p-[1%]">
+      <motion.p
+        className="text-witl-purple-100 text-5xl md:text-9xl absolute left-[4%] top-[65%] md:top-[50%] p-[1%]"
+        variants={animation}
+        transition={{ delay: 0.3 }}
+        initial="hidden"
+        whileInView="show"
+      >
         EVENTS
-      </p>
+      </motion.p>
     </div>
   );
-};
+}
 
 export default Header;
