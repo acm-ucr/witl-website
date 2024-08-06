b51657b3b0d8d71bed0cba89e5a94d43939ab7ed
Andrew Trang <andrewtrang@Andrews-MacBook-Air-6.local>
2024-08-06 00:23:26 -0700

    image animation problem

--- src/components/events/EventsHeader.jsx
+++ src/components/events/EventsHeader.jsx
@@ -5,20 +5,34 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 const animation = {
-  hidden: { opacity: 0, x: -30 },
+  hidden: { opacity: 0, y: -30 },
+  show: { opacity: 1, y: 0 },
+};
+
+const animationImage = {
+  hidden: { opacity: 0, x: 30 },
   show: { opacity: 1, x: 0 },
 };
 
 function Header() {
   return (
     <div className="bg-witl-purple-300 flex justify-end relative mt-[5%] py-[5%]">
-      <Image
-        src={networkingdinner}
-        alt="networking dinner"
-        className="w-[70%] h-auto pt-16 md:pt-0 object-cover"
-      />
+      <motion.div
+        variants={animationImage}
+        transition={{ delay: 0.3 }}
+        initial="hidden"
+        whileInView="show"
+      >
+        {" "}
+        <Image
+          className="w-[70%] object-cover h-auto pt-16 md:pt-0 md:w-[100%]"
+          src={networkingdinner}
+          alt="networking dinner"
+        />{" "}
+      </motion.div>
+
       <motion.p
-        className="text-witl-purple-100 text-5xl md:text-9xl absolute left-[4%] top-[65%] md:top-[50%] p-[1%]"
+        className="text-witl-purple-100 text-5xl md:text-9xl absolute left-[4%] top-[75%] md:top-[50%] p-[1%]"
         variants={animation}
         transition={{ delay: 0.3 }}
         initial="hidden"
