3cb01f7918d3e3796363fe7b82f95603c30d232d
Stanley Lew <stanley43595@gmail.com>
2024-08-11 14:35:21 -0700

    things

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -24,14 +24,14 @@ const BoardHeader = () => {
         whileInView="show"
       >
         <Image
-          className="object-cover h-auto w-[60%] mt-[20%] md:mt-[15%] "
+          className="object-cover h-auto w-[60%] mt-[20%] sm:mt-[15%] md:mt-[13%] lg:mt-[8%] 2xl:mt-[5%]"
           src={BoardIMG}
           alt="Board"
         />
       </motion.div>
 
       <motion.p
-        className="text-witl-purple-100 w-min leading-none z-0 absolute text-[8vw] left-[5%] top-[19.5%] md:top-[53%]"
+        className="text-witl-purple-100 w-min leading-none z-0 absolute text-[8vw] left-[5%] top-[19.5%] sm:top-[26%] md:top-[30%] lg:top-[35%] xl:top-[53%]"
         variants={animation}
         transition={{ delay: 0.3 }}
         initial="hidden"
