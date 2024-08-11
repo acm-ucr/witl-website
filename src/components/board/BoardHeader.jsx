f0fce5b5c390aa9d0b0e92801f0f5f1a91889c6c
Stanley Lew <stanley43595@gmail.com>
2024-08-11 14:03:49 -0700

    change

--- src/components/board/BoardHeader.jsx
+++ src/components/board/BoardHeader.jsx
@@ -31,7 +31,7 @@ const BoardHeader = () => {
       </motion.div>
 
       <motion.p
-        className="text-witl-purple-100 w-min leading-none z-0 absolute text-[8vw] left-[5%] top-[19.5%] sm:top-[26%] md:top-[30%] lg:top-[35%] xl:top-[43%] 2xl:top-[53%]"
+        className="text-witl-purple-100 w-min leading-none z-0 absolute text-[8vw] left-[5%] top-[19.5%] sm:top-[26%] md:top-[30%] lg:top-[35%] xl:top-[53%]"
         variants={animation}
         transition={{ delay: 0.3 }}
         initial="hidden"
