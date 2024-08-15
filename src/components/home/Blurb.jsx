4cf28dc9ba30f2a191651db3a3ce60801184cb18
justin-h-im <imjustin679@gmail.com>
2024-08-15 16:18:09 -0700

    fixed view on laptop screens

--- src/components/home/Blurb.jsx
+++ src/components/home/Blurb.jsx
@@ -16,8 +16,6 @@ const animate = {
   },
 };
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>;
-
 const Blurb = () => {
   return (
     <div className="bg-witl-purple-300 md:min-h-[1200px] max-h-screen flex justify-center items-center">
@@ -26,7 +24,7 @@ const Blurb = () => {
         initial="start"
         whileInView="end"
         transition={{ delay: 0.2 }}
-        className="bg-witl-white-100 rounded-[2rem] md:rounded-[6rem] md:scale-60 md:p-0 md:pb-10 md:-my-[10%] w-10/12"
+        className="bg-witl-white-100 rounded-[2rem] md:rounded-[6rem] md:scale-60 md:p-0 md:pb-10 w-10/12 4xl:pb-40"
       >
         <div className="grid grid-flow-row-dense grid-cols-6">
           <motion.div
@@ -50,7 +48,7 @@ const Blurb = () => {
                 initial="start"
                 whileInView="end"
                 transition={{ delay: 0.25 }}
-                className="flex flex-col tracking-tighter font-marcellus-sc text-2xl md:text-[8rem] md:ml-4 mt-8 md:mt-0 md:pt-24 leading-none"
+                className="flex flex-col tracking-tighter font-marcellus-sc text-2xl md:text-[8rem] lg:text-[6rem] 2xl:text-[8rem] md:ml-4 mt-8 md:mt-0 md:pt-24 leading-none"
               >
                 Womxn in
                 <br />
@@ -61,7 +59,7 @@ const Blurb = () => {
                 initial="start"
                 whileInView="end"
                 transition={{ delay: 0.3 }}
-                className="text-right tracking-tighter md:tracking-wide text-xs 2xl:text-[2.5rem] -mt-2 md:-mt-14 mr-6 md:mr-8 md:pl-10 md:pr-20 leading-tight lg:leading-normal"
+                className="text-right tracking-tighter md:tracking-wide text-xs lg:text-xl 2xl:text-[2.5rem] -mt-2 md:-mt-14 lg:-mt-8 mr-6 md:mr-8 md:pl-10 md:pr-20 leading-tight lg:leading-relaxed"
               >
                 <span>
                   stands <br /> as an organization catered to the diverse
@@ -76,13 +74,13 @@ const Blurb = () => {
             initial="start"
             whileInView="end"
             transition={{ delay: 0.35 }}
-            className="col-span-2 place-items-stretch justify-start m-5 md:m-10 md:pt-4 mb-4 md:mb-20 mt-0 md:mt-40 max-h-96 max-w-screen pt-2"
+            className="col-span-2 place-items-stretch justify-start m-5 md:m-10 md:pt-4 mb-4 md:mb-20 mt-0 md:mt-40 lg:mt-0 max-h-96 max-w-screen pt-2"
           >
             {" "}
             <Image
               src={teamphoto2}
               alt="teamphoto2"
-              className="w-full h-auto md:h-full object-cover rounded md:rounded-3xl mt-0 md:mt-10 scale-110 md:scale-100"
+              className="w-full h-auto md:h-full object-cover rounded md:rounded-3xl mt-0 3xl:mt-10 4xl:mt-40 scale-110 md:scale-100"
             />{" "}
           </motion.div>
           <motion.div
@@ -90,13 +88,13 @@ const Blurb = () => {
             initial="start"
             whileInView="end"
             transition={{ delay: 0.35 }}
-            className="col-span-4 place-items-stretch justify-start m-5 md:m-10 md:pt-4 mb-4 md:mb-20 mt-0 md:mt-40 max-h-96 max-w-screen pt-2"
+            className="col-span-4 place-items-stretch justify-start m-5 md:m-10 md:pt-4 mb-4 md:mb-20 mt-0 md:mt-40 lg:mt-0 max-h-96 max-w-screen pt-2"
           >
             {" "}
             <Image
               src={teamphoto3}
               alt="teamphoto3"
-              className="w-full h-auto md:h-full object-cover rounded md:rounded-3xl mt-0 lg:mt-10 scale-110 md:scale-100"
+              className="w-full h-auto md:h-full object-cover rounded md:rounded-3xl mt-0 3xl:mt-10 4xl:mt-40 scale-110 md:scale-100"
             />{" "}
           </motion.div>
         </div>
